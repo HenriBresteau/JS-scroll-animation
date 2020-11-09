831d80597055a86e35ac85099d8b0e835765b83a
henriB <henri.bretseau@gmail.com>
2020-11-09 11:10:44 +0100

    Img appear

---
+++ script.js
@@ -0,0 +1,18 @@
+const imgImprovise = document.getElementById("img-improvise");
+const popup = document.getElementById('pop-up');
+
+window.addEventListener('scroll', function () {
+    
+    // console.log( window.innerHeight ); Valeur hauteur page à partir du nav
+    // console.log( window.scrollY );   Valeur scroll
+    // console.log( document.body.offsetHeight ); Valeur total de la page
+
+    scrollValue = (window.innerHeight+ window.scrollY) /(document.body.offsetHeight);
+    console.log(scrollValue);
+
+    // IMG APPEAR
+    if ( scrollValue > 0.45  ) {
+        imgImprovise.style.opacity = '1';
+        imgImprovise.style.transform = 'none';
+    }
+})
